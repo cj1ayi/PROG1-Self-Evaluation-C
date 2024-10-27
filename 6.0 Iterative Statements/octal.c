0416122e638017e8eacaba7284a0488948a45699
Nate Tan <69299982+cj1ayi@users.noreply.github.com>
2024-10-27 17:27:43 +0800

    Push

--- 6.0 Iterative Statements/octal.c
+++ 6.0 Iterative Statements/octal.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 
-void octalN(int n)
+void displayOctal(int fInput)
 {
-    for (int i = 0; i <= n; i++)
+    int i;
+    // Main iteration loop
+    for (i = 0; i <= fInput; i++)
     {
-        int octalNum = 0;
-        int placeValue = 1;
-        int num = i;
+        int nNum = i; // Holds the value of i to be converted to octal
+        int nOctal = 0; // Variable for Octal representation of the number
+        int nPlace = 1; // Place of the digit of current digit in the octal number
 
-        while (num > 0)
+        // Loop to convert input to an octal 
+        while (nNum > 0)
         {
-            int remainder = num % 8;
-            octalNum += remainder * placeValue;
-            num /= 8;
-            placeValue *= 10;
+            int nRemainder = nNum % 8; /* Get the remainder of nNum when divided by 8 to 
+                                        extract the rightmost digit of the octal.*/
+            nOctal += nRemainder * nPlace;  /* Add the value of nOctal with the 
+                                            remainder times the updated place value if any */
+            nNum /= 8; // Update nNum with the quotient by dividing nNum by 8 
+            nPlace *= 10; // Moves to next place when multiplied by 10. 
+                          // Updates during every iteration of the while loop.
         }
-
-        printf("%d ", octalNum);
+        // Print the octal numbers each with one space to the right
+        printf("%d ", nOctal);
     }
     printf("\n");
 }
 
 int main()
 {
-    int n;
-    printf("n: ");
-    scanf("%d", &n);
+    // Input decimal number
+    float fInput;
+    scanf("%f", &fInput);
+    // Call function
+    displayOctal(fInput);
 
+    return 0;
 }
